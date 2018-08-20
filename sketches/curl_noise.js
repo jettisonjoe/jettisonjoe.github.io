d3f5f20aab63070e7a7caa7159e2a627b48c92c0
Joe Ethier <joeethier@gmail.com>
2018-08-19 17:30:22 -0700

    Evolve curl noise sketch.

--- sketches/curl_noise.js
+++ sketches/curl_noise.js
@@ -1,21 +1,18 @@
 /* global p5 */
-////////////////////////////////////////////////////////////////////////////////
-// CURL NOISE LOGIC
+
 var sketch = function (p) {
-    var
-        title = 'curl',
-        winW, winH,
-        noiseScale = 0.001,
+    const NOISE_SCALE = 0.001;
+    const MAX_PARTICLE_COUNT = 2000;
+    const PARTICLE_SIZE = 16;
+    const PARTICLE_SPEED = 2;
+    const MARGIN = 0;
+
+    var winW, winH,
         particles = [],
-        polarities = [],
         ages = [],
-        maxParticles = 10000,
-        maxSpeed = 2,
-        barrier,
-        barrierSize,
+        polarities = [],
         emitter;
-    ////////////////////////////////////////////////////////////////////////////
-    // Sets up sketch.
+
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
         var seed = (Math.random() * 4294967296)  >>> 0;
@@ -23,106 +20,132 @@ var sketch = function (p) {
         p.colorMode(p.HSB, 360, 100, 100, 1);
         winW = p.windowWidth;
         winH = p.windowHeight;
-        setSizes();
+        emitter = p.createVector(MARGIN + Math.random() * (winW - 2 * MARGIN),
+                                 MARGIN + Math.random() * (winH - 2 * MARGIN));
     };
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Draws.
     p.draw = function () {
-        // p.background(0, 0.02);
-        if (particles.length < maxParticles) {
+        if (particles.length < MAX_PARTICLE_COUNT) {
             particles.push(emitter.copy());
-            polarities.push(1);
             ages.push(1000);
         }
-        p.noFill();
-        p.stroke((p.frameCount/10) % 360, 100, 100, 1);
+        p.noStroke();
+        p.fill((p.frameCount/10) % 360, 100, 100, 1);
         for (var i = 0; i < particles.length; i++) {
-            var loc = particles[i],
-                vel = curl(loc.x, loc.y, p.frameCount).setMag(maxSpeed);
-            vel.mult(polarities[i]);
+            var loc = particles[i];
+            var vel = curl(loc.x,
+                           loc.y,
+                           p.frameCount,
+                           rampedPerlinPotential,
+                           p).setMag(PARTICLE_SPEED);
             loc.add(vel);
             // contain(loc, i);
-            p.ellipse(loc.x, loc.y, 2, 2);
+            p.rect(loc.x - PARTICLE_SIZE / 2,
+                   loc.y - PARTICLE_SIZE / 2,
+                   PARTICLE_SIZE,
+                   PARTICLE_SIZE);
             if (ages[i] <= 0) {
                 particles[i] = emitter.copy();
-                polarities[i] = 1;
                 ages[i] = 1000;
             } else { ages[i] -= 1 }
         }
     };
-    ////////////////////////////////////////////////////////////////////////////
-    // Window resizing logic
+
     p.windowResized = function () {
         winW = p.windowWidth;
         winH = p.windowHeight;
         p.resizeCanvas(winW, winH);
-        setSizes();
-        particles = [];
-        polarities = [];
-        ages = [];
+        winW = p.windowWidth;
+        winH = p.windowHeight;
+        emitter = p.createVector(Math.random() * winW, Math.random() * winH);
     };
-    ////////////////////////////////////////////////////////////////////////////
-    // Sets barrier and emitter info based on window size.
-    function setSizes() {
-        barrier = p.createVector(winW/2, winH/2);
-        barrierSize = p.min(winW - 50, winH - 50);
-        emitter = p.createVector(winW/2, winH/2);
+
+    /**
+    * Find the curl of the gradient of a potential field via finite difference.
+    *
+    * This function is designed for use in p5js sketches, and thus requires a
+    * p5js drawing context to be passed in, which is used to get the Vector
+    * creation function.
+    *
+    * @param [x] The x component of location to sample potential.
+    * @param [y] The y component of the location to sample potential.
+    * @param [t] The time component used to sample potential.
+    * @param [pFunc] The potential field function. Takes 4 args, the first 2
+    * representing an (x, y) coordinate in 2D sample space, the 3rd representing
+    * time, and the 4th being a p5js drawing context. Should return the
+    * potential at the corresponding point in 2D space-time.
+    * @param [p5Context] A p5js drawing context.
+    * @param [epsilon] Optional epsilon (infinitesimal) value; defaults to 1.
+    * @return A vector representing the curl at the given location.
+    */
+    function curl(x, y, t, pFunc, p5Context, epsilon = 1) {
+        var n1, n2, a, b;
+        // Change in x with regard to y.
+        n1 = pFunc(x, (y + epsilon), t, p5Context);
+        n2 = pFunc(x, (y - epsilon), t, p5Context);
+        a = (n1 - n2)/(2 * epsilon);
+        // Change in y with regard to x.
+        n1 = pFunc((x + epsilon), y, t, p5Context);
+        n2 = pFunc((x - epsilon), y, t, p5Context);
+        b = (n1 - n2)/(2 * epsilon);
+        return p5Context.createVector(a, -b);
     }
-    ////////////////////////////////////////////////////////////////////////////
-    // Uses finite difference method to compute curl of a gradient of a
-    // potential field.
-    function curl(x, y, t) {
-        var eps = 1,
-            n1, n2, a, b;
-        // Change in x wrt y.
-        n1 = rampedPotential(x, (y + eps), t);
-        n2 = rampedPotential(x, (y - eps), t);
-        a = (n1 - n2)/(2 * eps);
-        // Change in y wrt x.
-        n1 = rampedPotential((x + eps), y, t);
-        n2 = rampedPotential((x - eps), y, t);
-        b = (n1 - n2)/(2 * eps);
-        return p.createVector(a, -b);
+
+    /**
+    * Return a patterned random potential for a position in 2D space-time.
+    *
+    * @param [x] The x coordinate of a point in p5js's drawing space.
+    * @param [y] The y coordinate of a point in p5js's drawing space.
+    * @param [t] The time component; meant for use with p5js's frameCount.
+    * @param [p5js] A p5js drawing context.
+    */
+    function rampedPerlinPotential(x, y, t, p5js) {
+      var k = 0.001,
+          margin = 16,
+          v = p5js.createVector(x, y),
+          scaledNoise = p5js.noise(x * k, y * k, t * k);
+
+      var left = smoothRamp(p5js.map(x, 0, margin, 0, 1)),
+          right = smoothRamp(p5js.map(p5js.width - x, 0, margin, 0, 1)),
+          top = smoothRamp(p5js.map(y, 0, margin, 0, 1)),
+          bottom = smoothRamp(p5js.map(p5js.height - y, 0, margin, 0, 1));
+
+      return (scaledNoise * left * right * top * bottom);
     }
-    ////////////////////////////////////////////////////////////////////////////
-    // Computes value of Perlin noise at a coordinate.
-    function scaledNoise(x, y, t) {
-        return p.noise(x * noiseScale, y * noiseScale, t * noiseScale);
+
+    /**
+     * Smoothly ramp an input (from 0 to 1) from -1 to 1.
+     */
+    function smoothRamp(n) {
+      var r = (n * 2) - 1;
+      if (r >= 1) {
+        return 1;
+      }
+      if (r <= -1) {
+        return -1;
+      }
+      return ((15 / 8) * r
+              - (10 / 8) * Math.pow(r, 3)
+              + (3 / 8) * Math.pow(r, 5));
     }
-    ////////////////////////////////////////////////////////////////////////////
-    // Modulates the potential field by applying a ramp through zero based on
-    // distance to the closest boundary point.
-    function rampedPotential(x, y, t) {
-        return scaledNoise(x, y, t) * ramp(x, y);
-    }
-    ////////////////////////////////////////////////////////////////////////////
-    // Smoothly ramps through zero based on distance to the closest boundary
-    // point and width of the modified region of Perlin space (noiseScale).
-    function ramp(x, y) {
-        var v = barrier.copy().sub(x, y),
-            d = Math.min(x, winW - x, y, winH - y),
-            // d = v.mag()-barrierSize/2,
-            r = d/noiseScale;
-        if (r >= 1) { return 1; }
-        if (r <= -1) { return -1; }
-        return (15/8)*r - (10/8)*Math.pow(r, 3) + (3/8)*Math.pow(r, 5);
-    }
-    ////////////////////////////////////////////////////////////////////////////
-    // Keeps particles inside the barrier.
+
+    /**
+    * Keep particles inside the barrier.
+    */
     function contain(v, i) {
-        var d = p5.Vector.sub(barrier, v);
-        // Check if particle has moved outside boundary.
-        if (d.x < 0
-            || d.x > winW
-            || d.y < 0
-            || d.y > winH) {
-        // if (d.mag() >= barrierSize/2) {
-            // Move particle back inside boundary.
-            d.setMag(maxSpeed + 1);
-            v.add(d);
-            // Reverse particle's directional polarity.
-            polarities[i] *= -1;
+        var d = p.createVector(0, 0);
+        // Check if particle has moved outside of one of the side boundaries.
+        if (v.x <= MARGIN) {
+          d.add(p.createVector(MARGIN - v.x, 0));
+        } else if (v.x >= winW - MARGIN) {
+          d.add(p.createVector(v.x, 0));
         }
+        // Check if particle has moved outside of the top or bottom boundary.
+        if (v.y <= MARGIN) {
+          d.add(p.createVector(0, MARGIN - v.y));
+        } else if (v.y >= winH - MARGIN) {
+          d.add(p.createVector(0, v.y));
+        }
+        v.add(d);
     }
 };
