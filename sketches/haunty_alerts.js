74e4c2275ce46664616f19dc9bc28a4ece09293f
Joe Ethier <joeethier@gmail.com>
2018-10-15 15:06:11 -0700

    Change font and text effects.

--- sketches/haunty_alerts.js
+++ sketches/haunty_alerts.js
@@ -8,7 +8,7 @@
 class HauntyAlert extends StreamAlert {
   static get FONT_SIZE() { return 80; }
   static get MAX_FONT_SIZE() { return 800; }
-  static get MIN_FONT_SIZE() { return 10; }
+  static get MIN_FONT_SIZE() { return 20; }
 
   static get T_HOLD() { return 1000; }
   static get T_FADE_OUT() { return 2450; }
@@ -16,24 +16,32 @@ class HauntyAlert extends StreamAlert {
 
   static preload(p) {
     HauntyAlert.SOUND = p.loadSound('assets/sounds/werewolf.mp3');
-    HauntyAlert.FONT = p.loadFont('assets/fonts/Creepster-Regular.ttf');
-    HauntyAlert.FONT_COLOR_STRING = 'rgba(255, 255, 255, 0)';
+    HauntyAlert.FONT = p.loadFont('assets/fonts/PermanentMarker-Regular.ttf');
+    HauntyAlert.RED_COLOR_STRING = 'rgb(254,21,107)';
+    HauntyAlert.WHITE_COLOR_STRING = 'rgb(255,255,226)';
+    HauntyAlert.BLUE_COLOR_STRING = 'rgb(0,176,255)';
   }
 
   constructor(donorName, amount) {
     super();
     this.donorName = donorName;
-    this.amount = amount;
+    this.amount = Math.floor(amount).toFixed(0);
     this._sound = HauntyAlert.SOUND;
-    this._textColor = null;
+    this._textAlpha = null;
     this._textSize = HauntyAlert.FONT_SIZE;
+    this._red = null;
+    this._white = null;
+    this._blue = null;
   }
 
   update(p) {
     if (this.isComplete) { return; }
     if (this._t == 0) {
       // First frame of the alert.
-      this._textColor = p.color(HauntyAlert.FONT_COLOR_STRING);
+      this._red = p.color(HauntyAlert.RED_COLOR_STRING);
+      this._white = p.color(HauntyAlert.WHITE_COLOR_STRING);
+      this._blue = p.color(HauntyAlert.BLUE_COLOR_STRING);
+      this._textAlpha = 0;
       this._sound.play();
     }
     if (this._t < HauntyAlert.T_HOLD) {
@@ -42,35 +50,42 @@ class HauntyAlert extends StreamAlert {
           this._t,
           0, HauntyAlert.T_HOLD,
           HauntyAlert.MAX_FONT_SIZE, HauntyAlert.FONT_SIZE);
-      this._textColor.setAlpha(p.map(
+      this._textAlpha = p.map(
           this._t,
           0, HauntyAlert.T_HOLD,
-          0, 255));
+          0, 255);
     } else if (this._t < HauntyAlert.T_FADE_OUT) {
       // Hold period.
       this._textSize = HauntyAlert.FONT_SIZE;
-      this._textColor.setAlpha(255);
+      this._textAlpha = 255;
     } else if (this._t < HauntyAlert.T_COMPLETE) {
       // Fade-out period.
       this._textSize = p.map(
           this._t,
           HauntyAlert.T_FADE_OUT, HauntyAlert.T_COMPLETE,
           HauntyAlert.FONT_SIZE, HauntyAlert.MIN_FONT_SIZE);
-      this._textColor.setAlpha(p.map(
+      this._textAlpha = p.map(
           this._t,
           HauntyAlert.T_FADE_OUT, HauntyAlert.T_COMPLETE,
-          255, 0));
+          255, 0);
     } else {
       // Complete.
-      this._textColor.setAlpha(0);
+      this._textAlpha = 0;
       this._sound.stop();
     }
-    p.fill(this._textColor);
     p.textAlign(p.CENTER, p.CENTER);
     p.textFont(HauntyAlert.FONT);
     p.textSize(this._textSize);
+    this._red.setAlpha(this._textAlpha);
+    p.fill(this._red);
     p.text(
-        this.donorName + '\n$' + Math.floor(this.amount).toFixed(0),
+        this.donorName + '\n$' + this.amount,
+        p.windowWidth/2 + this._textSize / 20,
+        p.windowHeight/2 + this._textSize / 20);
+    this._white.setAlpha(this._textAlpha);
+    p.fill(this._white);
+    p.text(
+        this.donorName + '\n$' + this.amount,
         p.windowWidth/2,
         p.windowHeight/2);
     super.update(p);
@@ -115,7 +130,7 @@ var sketch = function (p) {
     alertQueue.update(p);
 
     // DEBUG
-    if (p.frameCount == 200) {
+    if (p.frameCount == 2) {
       alertQueue.enqueue(new HauntyAlert('Griggle', 100.0));
       alertQueue.enqueue(new HauntyAlert('Merph', 200.5));
     }
