144906f48b43f738dbbcf70ebd2e491d1ab24dc3
Joe Ethier <joeethier@gmail.com>
2018-10-28 23:19:18 -0700

    Add sky texture and improve palm tree.

--- sketches/beachcam.js
+++ sketches/beachcam.js
@@ -1,7 +1,8 @@
 var sketch = function (p) {
   var gradient,
       starfield,
-      palmtree;
+      palmtree,
+      lightOrigin;
 
   p.preload = function() {
     palmtree = p.loadImage('assets/img/palm_silhouette.png');
@@ -20,11 +21,20 @@ var sketch = function (p) {
         p,
         0, 0,
         p.max(p.windowWidth, p.windowHeight) * 2, 0.4, 0.03);
+    lightOrigin = p.createVector(0, 0);
+    lightOrigin.x = p.random(p.windowWidth);
+    lightOrigin.y = p.random(p.windowHeight);
   };
 
   p.draw = function () {
     gradient.draw(p);
     starfield.draw(p);
+    p.ellipseMode(p.CENTER);
+    p.noStroke();
+    p.fill(255, 255, 255, 1);
+    for (var i = p.windowWidth * 1.5; i >= 0; i = i - 32) {
+      p.ellipse(lightOrigin.x, lightOrigin.y, i, i);
+    }
     p.image(palmtree, 20, p.windowHeight - palmtree.height);
   };
 
