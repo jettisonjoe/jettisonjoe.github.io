84695f2e46c125ae8a19de78ceac39917177901f
Joe Ethier <joeethier@gmail.com>
2020-01-12 13:48:24 -0800

    Finish general cleanup of beachcam sketch.

--- sketches/beachcam.js
+++ sketches/beachcam.js
@@ -1,6 +1,11 @@
 var sketch = function (p) {
+  // How far up the screen the water goes. 0 is all sky, 1 is all water.
   const WATER_RATIO = 0.3;
-  const SCALE_DIVISOR = 3;
+  // Each "pixel" of the image will be this many real pixels across.
+  const PIXELATION = 3;
+  const FOREGROUND_IMAGE = 'assets/img/beach_silhouettes.png';
+  const STARFIELD_DENSITY = 0.8;
+  const STARFIELD_ROTATION_SPEED = 0.05;
   
   let gradient,
       starfield,
@@ -10,39 +15,40 @@ var sketch = function (p) {
       lightOrigin;
 
   p.preload = function() {
-    foreground = p.loadImage('assets/img/beach_silhouettes.png');
+    foreground = p.loadImage(FOREGROUND_IMAGE);
   }
 
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    let skyWidth = Math.ceil(p.windowWidth / SCALE_DIVISOR);
-    let skyHeight = Math.ceil(
-        ((1 - WATER_RATIO) * p.windowHeight) / SCALE_DIVISOR);
-    buffer = p.createGraphics(skyWidth, skyHeight);
+    p.imageMode(p.CORNER);
+    let width = p.min(p.windowWidth, foreground.width);
+    p.createCanvas(width, p.windowHeight);
+    let bufferWidth = Math.ceil(width / PIXELATION);
+    let bufferHeight = Math.ceil(
+        ((1 - WATER_RATIO) * p.windowHeight) / PIXELATION);
+    buffer = p.createGraphics(bufferWidth, bufferHeight);
     water = new Water(
         p,
         buffer,
-        Math.ceil(WATER_RATIO * p.windowHeight / SCALE_DIVISOR) + 1);
+        Math.ceil(WATER_RATIO * p.windowHeight / PIXELATION) + 1);
     gradient = new ScrollingGradient(
         p,
-        skyWidth, skyHeight,
-        (p.windowHeight / 3) * 2,
+        bufferWidth, bufferHeight,
+        (p.windowHeight / PIXELATION) * 2,
         createRandomBalancedScale(4));
-    p.imageMode(p.CORNER);
     starfield = new Starfield(
         p,
-        Math.sqrt(skyWidth * skyWidth + skyHeight * skyHeight),
-        0.8, 0.05);
-    lightOrigin = p.createVector(0, 0);
-    lightOrigin.x = p.random(buffer.width);
-    lightOrigin.y = p.random(buffer.height);
+        Math.sqrt(bufferWidth * bufferWidth + bufferHeight * bufferHeight),
+        STARFIELD_DENSITY,
+        STARFIELD_ROTATION_SPEED);
+    lightOrigin = p.createVector(
+        p.random(buffer.width),
+        p.random(buffer.height));
   };
 
   p.draw = function () {
     gradient.drawAt(buffer, 0, 0);
     starfield.drawAt(buffer, 0, 0);
 
-    buffer.stroke(0);
     buffer.ellipseMode(p.CENTER);
     buffer.noStroke();
     buffer.fill(255, 255, 255, 1);
@@ -52,14 +58,13 @@ var sketch = function (p) {
 
     p.push();
     p.noSmooth();
-    p.scale(3);
+    p.scale(PIXELATION);
     p.image(buffer, 0, 0);
     p.translate(0, buffer.height - 1);
     water.drawAt(p, 0, 0);
     p.pop();
 
     p.image(foreground, 0, p.windowHeight - foreground.height);
-    // console.log(p.frameRate());
   };
 
   p.windowResized = function () {
