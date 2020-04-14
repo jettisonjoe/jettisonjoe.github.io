1ab64ca2b0242ec963c7fe61c991342e200f0395
Joe Ethier <joeethier@gmail.com>
2020-04-13 20:57:32 -0700

    Add background texture to desktop sketch.

--- sketches/vintage_desktop.js
+++ sketches/vintage_desktop.js
@@ -1,5 +1,6 @@
 var sketch = function (p) {
   const BG_COLOR = '#657b83';
+  const BG_IMG_PATH = 'assets/img/vintage_bg_texture_1.png'
 
   const FONT_PATH = 'assets/fonts/gohufont-uni-11.ttf';
   const FONT_COLOR = '#93a1a1';
@@ -9,7 +10,8 @@ var sketch = function (p) {
   const ICON_MARGIN = 80;
   const NUM_ICONS = 2;
   const TEXT_OFFSET = 40;
-  const SELECTION_COLOR = '#073642';
+  const DARK_COLOR = '#073642';
+  const LIGHT_COLOR = '#93a1a1';
   const SELECTION_MARGIN = 6;
 
   const ICON_STRINGS = ['Joyport 1', 'Disk A'];
@@ -25,6 +27,7 @@ var sketch = function (p) {
 
   p.preload = function () {
     font = p.loadFont(FONT_PATH);
+    bg_img = p.loadImage(BG_IMG_PATH);
     for (var i = 0; i < NUM_ICONS; i++) {
       icons.push(p.loadImage(ICON_PATHS[i]));
     }
@@ -53,20 +56,27 @@ var sketch = function (p) {
     p.textSize(FONT_SIZE);
     p.textAlign(p.CENTER);
 
+    for (let y = 0; y < p.windowHeight; y += bg_img.height) {
+      for (let x = 0; x < p.windowWidth; x += bg_img.width) {
+        p.image(bg_img, x, y);
+      }
+    }
+
     for (var k = 0; k < NUM_ICONS; k++) {
       p.image(icons[k], xCoords[k], yCoords[k]);
+      p.fill(DARK_COLOR);
       if (k == selectedIconIdx) {
         var box = font.textBounds(ICON_STRINGS[k],
                                   xCoords[k] + ICON_SIZE/2,
                                   yCoords[k] + ICON_SIZE + TEXT_OFFSET,
                                   FONT_SIZE);
-        p.fill(SELECTION_COLOR);
+        p.fill(DARK_COLOR);
         p.rect(box.x - SELECTION_MARGIN - 1,
                box.y - SELECTION_MARGIN,
                box.w + 2 * SELECTION_MARGIN,
                box.h + 2 * SELECTION_MARGIN);
+        p.fill(LIGHT_COLOR);
       }
-      p.fill(FONT_COLOR);
       p.text(ICON_STRINGS[k],
              xCoords[k] + ICON_SIZE/2,
              yCoords[k] + ICON_SIZE + TEXT_OFFSET);
